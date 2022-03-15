8070008c61fe6c6c9c0d01bd982a3d8a68ea5160
benny.back <benny.back@kakaocorp.com>
2022-03-15 11:52:54 +0900

    '/' path로 들어오는 모든 파일요청은 클라이언트 빌드 폴더를 서빙하도록 변경

--- nasa-project/server/src/app.ts
+++ nasa-project/server/src/app.ts
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import cors from "cors";
 import planetsRouter from "./routes/planets/planets.router";
@@ -8,6 +9,9 @@ app.use(cors({
   origin: ['http://localhost:3000'],
 }));
 app.use(express.json());
+app.use(express.static(path.resolve(__dirname, '../public')));
+
 app.use(planetsRouter);
+app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, '../public/index.html')));
 
 export default app;
