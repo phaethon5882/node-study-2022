ca166a84c672955b0b10e70f5dfdb9ecb9307de4
benny.back <benny.back@kakaocorp.com>
2022-03-16 10:12:30 +0900

    [성능업] cluster 모듈(기본모듈임)을 사용해서 워커를 프로세서 개수만큼 늘려보자!

--- performance-example/src/server.ts
+++ performance-example/src/server.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import cluster from 'cluster';
+import os from 'os';
 
 const app = express();
 
@@ -10,13 +12,28 @@ function delay(duration: number) {
 }
 
 app.get('/', (req, res) => {
-  res.send('Performance example');
+  res.send(`Performance example ${process.pid}`);
 });
 
 app.get('/timer', (req, res) => {
   // delay the response
   delay(9000);
-  res.send('Ding ding ding!');
+  res.send(`Ding ding ding! ${process.pid}`);
 });
 
-app.listen(3000);
+
+console.log('Running server.js...');
+if (cluster.isPrimary) {
+  // 워커가 16개 돌아가니까 동시에 16개 처리를 할 수 있음.
+  // 논리 프로세서 개수, 나의 경우는 16(8 * 2)개
+  const NUM_WORKERS = os.cpus().length;
+
+  console.log('Master has been started...');
+  for (let i = 0; i < NUM_WORKERS; i++) {
+    cluster.fork();
+  }
+} else {
+  console.log('Worker process started...');
+  app.listen(3000);
+}
+
