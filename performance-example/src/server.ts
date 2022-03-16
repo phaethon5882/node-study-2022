9babff2c36ef930d8dc05ccd0b6d23db9ee8d928
benny.back <benny.back@kakaocorp.com>
2022-03-16 10:54:57 +0900

    나사프로젝트에도 pm2 를 적용해보자. db가 없어서 launch 정보가 동기화가 안 된다.

--- performance-example/src/server.ts
+++ performance-example/src/server.ts
@@ -15,12 +15,13 @@ app.get('/', (req, res) => {
 
 app.get('/timer', (req, res) => {
   // delay the response
-  delay(9000);
-  res.send(`Ding ding ding! ${process.pid}`);
+  delay(4000);
+  res.send(`Beep beep bepp! ${process.pid}`);
 });
 
 // pm2 를 쓰면 마스터 프로세스를 자동으로 생성해주고 워커 프로세스들이 -i 옵션으로 준만큼 생성된다.
 // pm2 start src/server.ts -i max -> 16개 워커 프로세스 생성됨!
+// pm2 reload 를 쓰면 다운타임 없이 롤링으로 하나씩 재시작해줌
 console.log('Running server.js...');
 console.log('Worker process started...');
 app.listen(3000);
