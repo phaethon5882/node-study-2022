166dc93161861759dc4004462ff77190d417ccfe
benny.back <benny.back@kakaocorp.com>
2022-03-16 09:50:55 +0900

    [성능업] 싱글스레드로 동작하는 이벤트 루프가 블록킹되면 나머지 처리들도 블록킹된다.

---
+++ performance-example/src/server.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+
+const app = express();
+
+function delay(duration: number) {
+  const startTime = Date.now();
+  while(Date.now() - startTime < duration) {
+    // event loop is blocked...
+  }
+}
+
+app.get('/', (req, res) => {
+  res.send('Performance example');
+});
+
+app.get('/timer', (req, res) => {
+  // delay the response
+  delay(9000);
+  res.send('Ding ding ding!');
+});
+
+app.listen(3000);
